236991191a725c6aa49dafcb94a39c253c61afee
HYEON(Company) <antaehyeon@live.co.kr>
2019-02-28 14:55:43 +0900

    Feat: apply Apollo Hooks to main code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import { ApolloProvider } from "react-apollo";
-import client from "./apolloClient";
-import Home from "./Home";
-import Detail from "./Detail";
+import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import client from './apolloClient';
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Router>
-          <main>
-            <Route exact={true} path={"/"} component={Home} />
-            <Route path={"/details/:movieId"} component={Detail} />
-          </main>
-        </Router>
+        <ApolloHooksProvider client={client}>
+          <Router>
+            <main>
+              <Route exact={true} path={'/'} component={Home} />
+              <Route path={'/details/:movieId'} component={Detail} />
+            </main>
+          </Router>
+        </ApolloHooksProvider>
       </ApolloProvider>
     );
   }
